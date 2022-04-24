7347cdc6de57f59a85d275ba9138239c1bc3b546
RodrigoCavalcantii <rodrigo.cavalcanti@liferay.com>
2022-04-24 01:33:34 -0300

    Index page | File structure cleanup

---
+++ react-app/src/routes/default/components/Presentation/Presentation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../styles/index.css"
+
+export default function Presentation() {
+  return (
+    <div className="center-div">
+      <h3>Unicap</h3>
+      <h2>Projeto Banco de Dados II</h2>
+      <h3>Alunos:<br/>Renato Rabello<br/>Rodrigo Cavalcanti</h3>
+      <button className="btn-go">Go</button>
+    </div>
+  );
+}
